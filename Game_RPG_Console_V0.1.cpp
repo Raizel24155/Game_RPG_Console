0b53413a05ba77290c18e33ca883b35c86601980
Filix66 <104318215+Filix66@users.noreply.github.com>
2022-04-24 19:13:38 +0200

    Add files via upload

---
+++ Game_RPG_Console_V0.1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <string>
+#include <conio.h>
+#include <algorithm>
+#include <time.h>
+
+using namespace std;
+int main()
+{
+    cout << "Hello World!\n";
+}
---
+++ Warrior.h
@@ -0,0 +1,9 @@
+#pragma once
+class Warrior
+{
+	int dmg;
+	int hp;
+	int ar;
+	int mr;
+};
+
